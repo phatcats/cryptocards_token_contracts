3e1fb86b09bbe0450cf819f89eafc68e115c21b1
Phat Cats <rob@phatcats.co>
2019-03-04 14:49:03 -0500

    Update to fix issue noted in Contract Audit by Callisto

--- contracts/CryptoCardsERC20.sol
+++ contracts/CryptoCardsERC20.sol
@@ -26,4 +26,13 @@ contract CryptoCardsERC20 is Ownable, ERC20Detailed, ERC20Capped {
         ERC20Detailed(name, symbol, decimals)
         ERC20Capped(tokenCap)
     {}
+
+    // Avoid 'Double Withdrawal Attack"
+    // see:
+    //  - https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM/
+    //  - https://docs.google.com/document/d/1Feh5sP6oQL1-1NHi-X1dbgT3ch2WdhbXRevDN681Jv4/
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(this));
+        super._transfer(from, to, value);
+    }
 }
